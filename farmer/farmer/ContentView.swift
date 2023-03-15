c6e4b017731d207761e6a9ff3a18c565e91480d5
arina krumm <92879566+arinaaaaaaa@users.noreply.github.com>
2023-03-15 15:52:18 +0300

    init app and create launch screen

---
+++ farmer/farmer/ContentView.swift
@@ -0,0 +1,6 @@
+//
+//  ContentView.swift
+//  farmer
+//
+//  Created by arina on 14.03.2023.
+
---
+++ farmer/farmer/farmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  farmerApp.swift
+//  farmer
+//
+//  Created by arina on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct farmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}

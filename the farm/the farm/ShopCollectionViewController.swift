e14515de13cd6fe8d198b0ebf148a62733530686
arina krumm <92879566+arinaaaaaaa@users.noreply.github.com>
2023-05-26 16:23:59 +0300

    complete shop page

--- the farm/the farm/ShopCollectionViewController.swift
+++ the farm/the farm/ShopCollectionViewController.swift
@@ -1,33 +1,53 @@
 import UIKit
 
 class ShopCollectionViewController: UICollectionViewController {
+    private let sectionInsets = UIEdgeInsets(top: 16, left: 20, bottom: 16, right: 20)
     
     // Создаем массив элементов коллекции
     var collectionItems: [CollectionItem] = [
-        CollectionItem(image: UIImage(named: "image1"), label1: "Label 1", label2: "Label 2"),
-        CollectionItem(image: UIImage(named: "image2"), label1: "Label 3", label2: "Label 4"),
-        CollectionItem(image: UIImage(named: "image3"), label1: "Label 5", label2: "Label 6")
+        CollectionItem(image: UIImage(named: "launch dog"), name: "Сиба-ину Тако", cost: "790"),
+        CollectionItem(image: UIImage(named: "launch dog"), name: "Сиба-ину Тако", cost: "790"),
+        CollectionItem(image: UIImage(named: "launch dog"), name: "Сиба-ину Тако", cost: "790"),
+        CollectionItem(image: UIImage(named: "launch dog"), name: "Сиба-ину Тако", cost: "790"),
+        CollectionItem(image: UIImage(named: "launch dog"), name: "Сиба-ину Тако", cost: "790"),
+        CollectionItem(image: UIImage(named: "launch dog"), name: "Сиба-ину Тако", cost: "790"),
+        CollectionItem(image: UIImage(named: "launch dog"), name: "Сиба-ину Тако", cost: "790")
     ]
     
-    //MARK: UICollectionViewDataSource
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let itemsPerRow: CGFloat = 2
+        let paddingWidth = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingWidth
+        let itemWidth = availableWidth / itemsPerRow
+        
+        layout.itemSize = CGSize(width: itemWidth, height: itemWidth*1.4)
+        layout.sectionInset = sectionInsets
+        layout.minimumLineSpacing = sectionInsets.left
+        layout.minimumInteritemSpacing = sectionInsets.left
+        
+        collectionView.collectionViewLayout = layout
+    }
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // Возвращает количество секций в коллекции
         return 1
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // Возвращает количество элементов в секции коллекции
         return collectionItems.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        // Создаем ячейку коллекции
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? MyCollectionViewCell else {
             fatalError("Unable to dequeue MyCollectionViewCell")
         }
         
-        // Настраиваем элементы ячейки
         let collectionItem = collectionItems[indexPath.row]
         cell.configure(with: collectionItem)
         
@@ -35,24 +55,105 @@ class ShopCollectionViewController: UICollectionViewController {
     }
 }
 
-//MARK: - UICollectionViewDelegateFlowLayout
+class CollectionItem {
+    var image: UIImage?
+    var name: String
+    var cost: String
+    
+    init(image: UIImage?, name: String, cost: String) {
+        self.image = image
+        self.name = name
+        self.cost = cost
+    }
+}
 
-extension ShopCollectionViewController: UICollectionViewDelegateFlowLayout {
+class MyCollectionViewCell: UICollectionViewCell {
     
-    // Настраиваем размер ячеек
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let padding: CGFloat = 16
-        let collectionWidth = collectionView.frame.width
-        let widthPerItem = (collectionWidth - padding) / 2
-        return CGSize(width: widthPerItem, height: widthPerItem)
+    let myImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    let name: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        nameLabel.textColor = .white
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        return nameLabel
+    }()
+    
+    let cost: UILabel = {
+        let costLabel = UILabel()
+        costLabel.font = UIFont.boldSystemFont(ofSize: 14)
+        costLabel.textColor = .white
+        costLabel.translatesAutoresizingMaskIntoConstraints = false
+        return costLabel
+    }() // add cost label
+    
+    let coinImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "coin")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    let buyButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(red: 1.0, green: 0.82, blue: 0.56, alpha: 1.0)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5.0
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(myImageView)
+        contentView.addSubview(name)
+        contentView.addSubview(buyButton)
+
+        buyButton.addSubview(cost)
+        buyButton.addSubview(coinImageView)
+        
+        NSLayoutConstraint.activate([
+            myImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+            myImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            myImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            myImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            myImageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.6),
+            myImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            name.topAnchor.constraint(equalTo: myImageView.bottomAnchor, constant: 8),
+            name.centerXAnchor.constraint(equalTo: centerXAnchor),
+
+            // Update constraints for cost and coinImageView
+            cost.centerYAnchor.constraint(equalTo: buyButton.centerYAnchor),
+            cost.trailingAnchor.constraint(equalTo: coinImageView.leadingAnchor, constant: -8),
+            coinImageView.centerYAnchor.constraint(equalTo: buyButton.centerYAnchor),
+            coinImageView.trailingAnchor.constraint(equalTo: buyButton.trailingAnchor, constant: -8),
+            
+            buyButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            buyButton.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 8),
+            buyButton.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.5),
+            buyButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            buyButton.heightAnchor.constraint(equalToConstant: 35)
+        ])
+        
+        contentView.backgroundColor = UIColor(red: 1.0, green: 0.886, blue: 0.737, alpha: 1.0)
+        contentView.layer.cornerRadius = 5.00
     }
     
-    // Настраиваем отступы между ячейками в ряду
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 16
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    func configure(with collectionItem: CollectionItem) {
+        myImageView.image = collectionItem.image
+        name.text = collectionItem.name
+        cost.text = collectionItem.cost // set cost label text
     }
 }
